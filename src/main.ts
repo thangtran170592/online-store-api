91c2e0ea0f3f09437e8584fd06a1ae57a53615c8
Thang Tran <152130526+thangtran170592@users.noreply.github.com>
2025-07-21 15:59:18 +0700

    Fist

---
+++ src/main.ts
@@ -0,0 +1,23 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+async function bootstrap(): Promise<void> {
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+  const config = new DocumentBuilder()
+    .setTitle('Online Store API')
+    .setDescription('API docs for Online Store')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  await app.listen(process.env.PORT ?? 3000);
+}
+bootstrap();
